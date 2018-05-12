a699aac96e053224794e6b16cf162cd1c8209374
codingFish <wangliang199538@live.com>
2018-05-12 16:45:04 +0800

    websocket server可以和proxy通信了

--- message.php
+++ message.php
@@ -7,30 +7,75 @@
  */
 
 //本机ip地址
-define('LOCALHOST', '192.168.3.195');
+define('LOCALHOST', '192.168.3.48');
 //fd映射登录名zSetKey
 define('MEMBER_FD_KEY', LOCALHOST.'_member');
 //定义proxy服务器IP
-define('PROXY_SERVER', '192.168.3.158');
+define('PROXY_SERVER', '192.168.3.46');
 //定义redis
-define('REDIS_SERVER', '192.168.3.158');
+define('REDIS_SERVER', '192.168.3.46');
 
 
 $server = new swoole_websocket_server("0.0.0.0", 9501);
 
+
 $redis = new Redis();
 $redis->connect(REDIS_SERVER);
 
-$client = new swoole_client(PROXY_SERVER, 9500);
-$client = new swoole_client(SWOOLE_SOCK_TCP, SWOOLE_SOCK_ASYNC);
+$client;
+
+//创建work和task进程时，创建一个tcp客户端，与proxy服务器通讯
+$server->on('WorkerStart', function (swoole_websocket_server $server, $workId) use ($redis, &$client) {
+    //在worker进程并且workId等于0建立和proxy服务器的tcp连接
+    //todo:保存一台服务器登录的用户数据结构不用ZSET
+    if (!$server->taskworker && $workId == 0){
+
+        $client = new swoole_client(SWOOLE_SOCK_TCP, SWOOLE_SOCK_ASYNC);
+
+
+
+        $client->on("connect", function($cli){
+            $cli->send("hello world\n");
+        });
+
+        $client->on("receive", function($cli, $data) use (&$server, $redis){
+
+            $connects = $redis->zRange(MEMBER_FD_KEY, 0, -1);
+
+            if (empty($connects)){
+                return;
+            }
+
+            foreach ($connects as $key=>$value){
+                $fd = $redis->zScore(MEMBER_FD_KEY, $value);
+                $server->push($fd, $data);
+            }
+
+        });
+
+        $client->on("error", function($cli){
+            echo "connect failed\n";
+        });
+
+        $client->on("close", function($cli){
+            echo "connection close\n";
+        });
+
+        $client->connect("192.168.3.46", 9500, 0.5);
+    }
+});
+
 
 //socket连接
-$server->on('open', function (swoole_websocket_server $server, $request) {
+$server->on('open', function (swoole_websocket_server $server, $request)  {
+    var_dump($server->connections);
     echo "握手成功{$request->fd}\n";
 });
 
 //socket收到消息
-$server->on('message', function (swoole_websocket_server $server, $frame) use ($redis) {
+$server->on('message', function (swoole_websocket_server $server, $frame) use ($redis, &$client) {
+
+    $client->send('总算拿到句柄了！');
 
     $data = json_decode($frame->data, true);
 
@@ -79,5 +124,7 @@ $server->on('close', function ($ser, $fd) use ($redis) {
     $redis->zRemRangeByScore(MEMBER_FD_KEY, $fd, $fd);
 
 });
+
+
 
 $server->start();
--- proxy.php
+++ proxy.php
@@ -6,26 +6,44 @@
  * Time: 16:15
  */
 
-$server = new swoole_server("127.0.0.1", 9500);
+$server = new swoole_server("0.0.0.0", 9500);
 
 
 $server->on('connect', function (swoole_server $server, $fd){
     echo "connection open: {$fd}\n";
 
-    $info = $server->getClientInfo();
+//    $info = $server->getClientInfo($fd);
 
-    var_dump($info);
+//    var_dump($info);
+
+    $server->send($fd, '连接成功');
 
 });
 
 
 $server->on('receive', function ($server, $fd, $reactor_id, $data) {
+
+    var_dump($data);
+
     $server->send($fd, "Swoole: {$data}");
 });
 
 
 $server->on('close', function ($server, $fd) {
     echo "connection close: {$fd}\n";
+});
+
+$server->on('WorkerStart', function (swoole_server $server){
+    if ($server->taskworker){
+        return;
+    }
+
+    swoole_timer_tick(10000, function () use ($server){
+        foreach ($server->connections as $fd){
+            $server->send($fd, '我是proxy');
+        }
+    });
+
 });
 
 
